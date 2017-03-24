cd3aa2668c89cbb5774cfb20f3d0e59e102210be
mhein <mhein@wavebroadband.com>
2017-03-24 15:28:02 -0700

    Added example Dial.

--- telnet_test.go
+++ telnet_test.go
@@ -233,3 +233,20 @@ func TestBuffer_ForwardUpToIACAndProcess(t *testing.T) {
 	tel.quit <- true
 	time.Sleep(time.Duration(50) * time.Millisecond)
 }
+
+func ExampleDial() {
+	// Dial the telnet server
+	conn, err := Dial("tcp", "rainmaker.wunderground.com")
+	if err != nil {
+		panic("Unable to connect.")
+	}
+	// Read 30 bytes from the stream
+	buf := make([]byte, 30)
+	_, err = conn.Read(buf)
+	if err != nil {
+		panic("Unable to read from stream.")
+	}
+
+	// Write 'Hello World' to the stream.
+	_, err = conn.Write([]byte("Hello world!"))
+}
